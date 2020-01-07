a770a738e9ea926f8d28ecef86cdbc7d8e1ae147
erikrios <erikriosetiawan49@gmail.com>
2020-01-07 11:03:20 +0700

    Use a ViewModelFactory

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.ScoreFragmentBinding
@@ -29,6 +31,10 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
 /**
  * Fragment where the final score is shown, after the game is over
  */
+
+private lateinit var viewModel: ScoreViewModel
+private lateinit var viewModelFactory: ScoreViewModelFactory
+
 class ScoreFragment : Fragment() {
 
     override fun onCreateView(
@@ -44,6 +50,12 @@ class ScoreFragment : Fragment() {
                 container,
                 false
         )
+
+        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(ScoreViewModel::class.java)
+
+        binding.scoreText.text = viewModel.score.toString()
 
         return binding.root
     }
