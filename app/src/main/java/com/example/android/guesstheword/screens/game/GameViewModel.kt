1663641b54fd3c19ce90a35bab655faf1d03965f
erikrios <erikriosetiawan49@gmail.com>
2020-01-27 19:10:01 +0700

    Change the score and word to use LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,21 +1,25 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
     // The current word
-    var word = ""
+    val word = MutableLiveData<String>()
 
     // The current score
-    var score = 0
+    val score = MutableLiveData<Int>()
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
 
     init {
+        word.value = ""
+        score.value = 0
+
         Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
